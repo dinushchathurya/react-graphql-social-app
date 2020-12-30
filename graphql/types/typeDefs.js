dec7bc34a8d56429324f9a294013c14ce9d03df7
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-30 21:57:16 +0530

    :sparkles: Create RegisterInput and User type

--- graphql/types/typeDefs.js
+++ graphql/types/typeDefs.js
@@ -2,13 +2,32 @@ const { gql } = require('apollo-server');
 
 module.exports = gql `
     type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
+        id:ID!
+        body:String!
+        createdAt:String!
+        username:String!
+    }
+
+    type User {
+        id:ID!
+        email:String!
+        token:String!
+        username:String!
+        createdAt:String!
+    }
+
+    input RegisterInput {
+        username:String!
+        email:String!
+        password:String!
+        confirmPassword:String!
     }
 
     type Query {
         getPosts:[Post]
+    }
+
+    type Mutation {
+        register(registerInput:RegisterInput) : User!
     }
 `;
