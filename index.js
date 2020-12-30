e9a7ff6a9530fa2cbf784007dfe0d8bec6602212
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-30 21:40:20 +0530

    :hammer: Peer resolvers and type to index.js

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
-
-const typeDefs = gql `
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-
-    type Query {
-        getPosts:[Post]
-    }
-`;
-
-const resolvers = {
-    Query: {
-       async getPosts() {
-           try {
-               const posts = await Post.find();
-               return posts;
-           } catch (err) {
-               throw new Error(err);
-           }
-       } 
-    }
-}
+const typeDefs = require('./graphql/types/typeDefs');
+const resolvers = require('./graphql/resolvers/index');
+const { mongoURI } = require('./config/keys');
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
-
-/* DataBase Config */
-const { mongoURI } = require('./config/keys');
 
 /* Connect to MongoDB */
 mongoose
