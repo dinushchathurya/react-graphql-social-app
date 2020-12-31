8b596c3a834c0368b2df90b6fba5e827ad68bcfa
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-31 22:06:59 +0530

    :sparkles: Create regEx to validate user register inputs

---
+++ utils/validators.js
@@ -0,0 +1,31 @@
+module.exports.validateRegisterInput = (
+    username,
+    email,
+    password,
+    confirmPassword
+ ) => {
+    const errors = {};
+    if(username.trim() === ''){
+        error.username = 'Username cannot be empty';
+    }
+
+    if(email.trim() === ''){
+        error.email = 'Email cannot be empty';
+    } else {
+        const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA])*@[0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+        if(!email.match(regEx)){
+            error.password = 'Password should be a valid password';
+        }
+    } 
+
+    if(password === ''){
+        errors.password = 'Password cannot be empty';
+    } else if(password !== confirmPassword){
+        errors.confirmPassword = 'Password mismatch';
+    }
+
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+    }
+}
