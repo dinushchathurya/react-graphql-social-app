b77cfbebd2bb1cb0e11734f9c0149b65e01337fe
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-31 22:08:15 +0530

    :hammer: Peer register input validator

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,18 +1,34 @@
 const bcrypt =require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } =require('apollo-server');
 
+const { validateRegisterInput } = require('../../utils/validators');
 const { SECRET_KEY } = require('../../config/keys_dev');
 const User= require('../../models/User');
 
 module.exports = {
     Mutation: {
         async register(__, {registerInput: {username,email,password,confirmPassword}}, context, info) {
+
+            const { valid, errors } = validateRegisterInput(username,email,password,confirmPassword);
+            if(!valid){
+                throw new UserInputError('Errors', { errors })
+            }
+            const user = await User.findOne({ username });
+            if(user) {
+                throw new UserInputError('Username is already taken', {
+                    errors:{
+                        username: 'This username is already taken'
+                    }
+                })
+            }
             password = await bcrypt.hash(password, 12);
 
             const newUser = new User({
                 email,
                 username,
                 password,
+                confirmPassword,
                 createdAt: new Date().toISOString()
             })
 
