62f9389281b38d5fec86ca3b6f1b37ee55ca0818
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-01 18:55:28 +0530

    :sparkles: Create login function and define general token generate function

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -6,8 +6,45 @@ const { validateRegisterInput, validateLoginInput } = require('../../utils/valid
 const { SECRET_KEY } = require('../../config/keys_dev');
 const User= require('../../models/User');
 
+function generateToken(user){
+    return jwt.sign({
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }, SECRET_KEY, { expiresIn: '1h' })
+}
+
 module.exports = {
     Mutation: {
+        async login (__, { username, password }) {
+
+            const { valid, errors } = validateLoginInput(username, password);
+
+            if (!valid) {
+                throw new UserInputError('Errors', { errors })
+            }
+            
+            const user = await User.findOne({ username });
+            if (!user) {
+                errors.general = 'User not found';
+                throw new UserInputError('User not found', { errors });
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if(!match){
+                errors.general = 'Invalid credentials';
+                throw new UserInputError('Invalid credentials', { errors });
+            }
+
+            const token = generateToken(user);
+
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+
+        },
         async register(__, {registerInput: {username,email,password,confirmPassword}}, context, info) {
 
             const { valid, errors } = validateRegisterInput(username,email,password,confirmPassword);
@@ -34,11 +71,8 @@ module.exports = {
 
             const res = await newUser.save();
 
-            const token = jwt.sign({
-                id:res.id,
-                email:res.email,
-                username:res.username
-            }, SECRET_KEY, { expiresIn: '1h'} )
+            const token = generateToken(res);
+
             return {
                 ...res._doc,
                 id: res._id,
