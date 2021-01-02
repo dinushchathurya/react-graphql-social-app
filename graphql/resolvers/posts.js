673ef458e0383d40df886a63880505efc174c4b6
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-02 16:25:00 +0530

    :sparkles: Create likePost mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const isAuth = require('../../utils/is-auth');
@@ -53,7 +53,26 @@ module.exports = {
             } catch (err) {
                 throw new Error(err);   
             }
-        }
+        },
+        async likePost(_, { postId }, context) {
+            const { username } = isAuth(context);
 
+            const post = await Post.findById(postId);
+            if (post) {
+                if (post.likes.find((like) => like.username === username)) {
+                    // Post already likes, unlike it
+                    post.likes = post.likes.filter((like) => like.username !== username);
+                } else {
+                    // Not liked, like post
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
     }
 }
