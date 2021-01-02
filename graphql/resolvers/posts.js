90a5dac58690e3cbaf0e07f5ed3817373f4fcaa7
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-02 17:50:21 +0530

    :sparkles: Create new post subscription

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -38,6 +38,11 @@ module.exports = {
             });
 
             const post = await newPost.save();
+
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            });
+
             return post;
         },
         async deletePost(_, { postId }, context){
@@ -60,10 +65,8 @@ module.exports = {
             const post = await Post.findById(postId);
             if (post) {
                 if (post.likes.find((like) => like.username === username)) {
-                    // Post already likes, unlike it
                     post.likes = post.likes.filter((like) => like.username !== username);
                 } else {
-                    // Not liked, like post
                     post.likes.push({
                         username,
                         createdAt: new Date().toISOString()
@@ -73,6 +76,11 @@ module.exports = {
                 await post.save();
                 return post;
             } else throw new UserInputError('Post not found');
+        },
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
 }
