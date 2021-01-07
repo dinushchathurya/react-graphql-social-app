30bb74c4483327dd1f09e6a914276894cadf42f4
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-07 20:58:45 +0530

    :sparkles: Peering pages for route path

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home  from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
   );
 }
 
