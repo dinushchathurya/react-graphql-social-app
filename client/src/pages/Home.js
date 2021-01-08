cad756df9ae114cf13a36d3f8433774f7136c352
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-08 21:25:35 +0530

    :hammer: Peering PostCard component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,21 +3,25 @@ import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { Grid, Image } from 'semantic-ui-react';
 
+import PostCard from '../components/PostCard';
 function Home() {
-    const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+    const { loading, data: {getPosts : posts}} = useQuery(FETCH_POSTS_QUERY)
 
-    if(data){
-        console.log(data)
-    }
     return (
         <Grid columns={3} divided>
             <Grid.Row>
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
-                <Grid.Column>
-                    <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
-                </Grid.Column>
+                {loading ? (
+                    <h1>Loading...</h1>
+                ) : (
+                    posts && posts.map(post =>(
+                        <Grid.Column key={post.id}>
+                           <PostCard post={post} />
+                        </Grid.Column>
+                    ))
+                )}
             </Grid.Row>
         </Grid>
     )
