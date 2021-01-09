c990e995add29aecc58c66c047cfccb5c3842353
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-09 12:56:55 +0530

    :lipstick: Add basic touchup to PostCard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,12 +4,13 @@ import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
 
 import PostCard from '../components/PostCard';
+
 function Home() {
     const { loading, data: {getPosts : posts}} = useQuery(FETCH_POSTS_QUERY)
 
     return (
-        <Grid columns={3} divided>
-            <Grid.Row>
+        <Grid columns={3}>
+            <Grid.Row className="page-title">
                 <h1>Recent Posts</h1>
             </Grid.Row>
             <Grid.Row>
@@ -17,7 +18,7 @@ function Home() {
                     <h1>Loading...</h1>
                 ) : (
                     posts && posts.map(post =>(
-                        <Grid.Column key={post.id}>
+                        <Grid.Column key={post.id} style={{marginBottom: 20}}>
                            <PostCard post={post} />
                         </Grid.Column>
                     ))
