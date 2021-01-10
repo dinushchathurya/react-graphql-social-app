4a41223cb3393dbb00336262719e1ae91987fee1
dinushchathurya <dinushchathurya21@gmail.com>
2021-01-10 22:38:17 +0530

    :sparkles: Peering Auth Context

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,13 @@
-import React ,{ useState } from 'react';
+import React, { useContext,useState } from 'react';
 import { Form, Button } from 'semantic-ui-react';
 import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
+import { AuthContext } from '../context/auth';
+
 function Register(props) {
+
+    const context = useContext(AuthContext)
 
     const [errors, setErrors] = useState({});
     const [values, setValues] = useState({
@@ -18,7 +22,8 @@ function Register(props) {
     }
 
     const [addUser, { loading }] = useMutation(REGISTER_USER, {
-        update(_, result) {
+        update(_, { data: { register: userData } }) {
+            context.login(userData)
             props.history.push('/')
         },
         onError(err){
